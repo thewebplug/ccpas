5eba39a0754845f3240cd6f88cb0f7e0e0c8b3bd
saleem.jibril <saleem.jibril@netzence.com>
2024-06-14 15:18:57 +0100

    updated menu component

--- app/components/menu.jsx
+++ app/components/menu.jsx
@@ -1,9 +1,10 @@
 export default function Menu({ items }) {
   return (
     <div className="menu">
-      {items?.map((item) => 
+      {items?.map((item, index) => 
       <div className="menu__inner"
       onClick={item?.function}
+      key={index}
       >
       {item?.name}
       {/* <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
